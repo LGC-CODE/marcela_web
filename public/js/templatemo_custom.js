6d10671f03fdba0dc6794d59925c12a6cbb3463b
LGC-CODE <luisconstante@yahoo.com>
2017-03-16 16:47:39 -0700

    finished overall layout

--- public/js/templatemo_custom.js
+++ public/js/templatemo_custom.js
@@ -13,4 +13,54 @@ $(document).ready(function(){
 	$('.overlay1 .glyphicon.glyphicon-remove').on('click', function(){
 		$('.overlay1').fadeOut(1000);
 	});
+
+  $('.owl-carousel').owlCarousel({
+	    rtl:false,
+	    autoplay: true,
+	    loop:true,
+	    margin:0,
+	    nav:false,   
+    	autoplayTimeout:3500,
+    	autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
 });
+
+function getinfo(){
+	var name = document.getElementById('f-name').value;
+	var message = document.getElementById('f-message').value;
+	var form = document.getElementById('form');
+
+	if(!name || !message){
+		alert('please fill out all fields');
+		return;
+	}
+
+	var subject = encodeURIComponent('I would like more information about your bartending service');
+	var body = encodeURIComponent(message);
+
+		var link = "mailto:luisconstante@yahoo.com?subject=";
+		link += subject;
+		link += '&body=';
+		link += body;
+		
+		form.reset();
+
+		window.location.href = link;
+
+}
+
+
+
+
+
